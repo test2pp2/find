ad1f7d817870d79d476fe4fa13b3f2628b086956
pp2 <test2.pp2@gmail.com>
2020-07-08 01:58:21 +0900

    .

---
+++ Classes/LobbyScene.cpp
@@ -0,0 +1,97 @@
+﻿#include "LobbyScene.h"
+#include <codecvt>
+
+USING_NS_CC;
+
+#if CC_TARGET_PLATFORM == CC_PLATFORM_WIN32
+#pragma execution_character_set("utf-8")
+#endif
+
+Scene* Lobby::createScene()
+{
+    return Lobby::create();
+}
+
+// Print useful error message instead of segfaulting when files are not there.
+static void problemLoading(const char* filename)
+{
+    printf("Error while loading: %s\n", filename);
+    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+}
+
+std::string wstring_to_utf8(const std::wstring & str)
+{
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
+    return myconv.to_bytes(str);
+}
+
+// on "init" you need to initialize your instance
+bool Lobby::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Scene::init() )
+    {
+        return false;
+    }
+
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    center_ = { origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2 };
+
+    /////////////////////////////
+    // 2. add a menu item with "X" image, which is clicked to quit the program
+    //    you may modify it.
+
+    // add a "close" icon to exit the progress. it's an autorelease object
+    auto closeItem = MenuItemImage::create(
+                                           "CloseNormal.png",
+                                           "CloseSelected.png",
+                                           CC_CALLBACK_1(Lobby::menuCloseCallback, this));
+
+    if (closeItem == nullptr ||
+        closeItem->getContentSize().width <= 0 ||
+        closeItem->getContentSize().height <= 0)
+    {
+        problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
+    }
+    else
+    {
+        float x = origin.x + visibleSize.width - closeItem->getContentSize().width/2;
+        float y = origin.y + closeItem->getContentSize().height/2;
+        closeItem->setPosition(Vec2(x,y));
+    }
+
+    // create menu, it's an autorelease object
+    auto menu = Menu::create(closeItem, NULL);
+    menu->setPosition(Vec2::ZERO);
+    this->addChild(menu, 1);
+
+   // StringUtils::UTF32ToUTF8(input, output);
+    Label* label = Label::createWithTTF("뭐가뭐지", "fonts/nanumb.ttf", 50);
+    if (label == nullptr) {
+        problemLoading("'fonts/Marker Felt.ttf'");
+    } else {
+        // position the label on the center of the screen
+        label->setPosition(center_);
+
+        // add the label as a child to this layer
+        this->addChild(label, 1);
+    }
+
+
+    return true;
+}
+
+
+void Lobby::menuCloseCallback(Ref* pSender)
+{
+    //Close the cocos2d-x game scene and quit the application
+    Director::getInstance()->end();
+
+    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() as given above,instead trigger a custom event created in RootViewController.mm as below*/
+
+    //EventCustom customEndEvent("game_scene_close_event");
+    //_eventDispatcher->dispatchEvent(&customEndEvent);
+
+}
---
+++ generate_sln.bat
@@ -0,0 +1,3 @@
+if not exist proj.win32-sln mkdir proj.win32-sln
+cd proj.win32-sln
+cmake .. -G"Visual Studio 15 2017" -A Win32
